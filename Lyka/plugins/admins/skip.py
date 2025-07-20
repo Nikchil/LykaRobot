c035baad43aef052330e0c0e353ccbf930110cd1
Nickhil <teamstormxd@gmail.com>
2025-07-20 13:11:47 +0530

    Update skip.py

--- Lyka/plugins/admins/skip.py
+++ Lyka/plugins/admins/skip.py
@@ -158,7 +158,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_6"])
         button = stream_markup(_, videoid, chat_id)
-        img = await get_thumb(videoid)
+        img = await gen_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -231,7 +231,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, videoid, chat_id)
-            img = await get_thumb(videoid)
+            img = await gen_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
