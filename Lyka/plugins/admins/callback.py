d3e9be51bdbec115a1d8352f1c6ef4c2613e9195
Nickhil <teamstormxd@gmail.com>
2025-07-20 13:13:32 +0530

    Update callback.py

--- Lyka/plugins/admins/callback.py
+++ Lyka/plugins/admins/callback.py
@@ -274,7 +274,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 return await mystic.edit_text(_["call_6"])
             button = stream_markup(_, videoid, chat_id)
-            img = await get_thumb(videoid)
+            img = await gen_thumb(videoid)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -349,7 +349,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["markup"] = "tg"
             else:
                 button = stream_markup(_, videoid, chat_id)
-                img = await get_thumb(videoid)
+                img = await gen_thumb(videoid)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
                     caption=_["stream_1"].format(
