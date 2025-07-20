6f85ca764e389b856cb0104a44b6993cbada85d0
Nickhil <teamstormxd@gmail.com>
2025-07-20 13:31:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/StormBeatz/Lyka",
+    "https://github.com/Nikchil/LykaRobot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
